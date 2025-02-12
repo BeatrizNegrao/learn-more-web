0302176bb72703434b2622abfa99fb4c09e70ff3
BeatrizNegrao <beatrizfranciscanegrao@gmail.com>
2025-02-12 17:32:48 -0300

    Code update - footer and modal

--- JAVASCRIPT/cadastro.js
+++ JAVASCRIPT/cadastro.js
@@ -1,12 +1,25 @@
 
-document.addEventListener("DOMContentLoaded", () => { 
-    const btnCadastrar= document.getElementById("btnCadastrar");
-    const cadastroModal = new bootstrap.Modal(document.getElementById("cadastroModal"));
+document.addEventListener("DOMContentLoaded", () => {
+    const loginModal = new bootstrap.Modal(document.getElementById('cadastroModal'));
 
-    btnCadastrar.addEventListener("click", () => {
-        cadastroModal.show();
+    // Exemplo de abertura do modal via código
+    const openModalButton =  document.querySelector('[data-bs-target="#cadastroModal"]');
+    openModalButton.addEventListener('click', () => {
+        loginModal.show();
     });
-});
 
+    // Login adicional, como validação do formulário 
+    const loginForm = document.querySelector('#cadastroModal form');
+    loginForm.addEventListener('submit', (event) => {
+        event.preventDefault(); //evita o envio padrão do formulário 
+        const name = cadastroForm.querySelector('#name').value;
+        const email = cadastroForm.querySelector('#email').value;
+        const password = cadastroForm.querySelector('#password').value; 
+
+        console.log(`Name: ${name}, Email: ${email}, Password: ${password}`);
+
+    });
+
+});
 
 
